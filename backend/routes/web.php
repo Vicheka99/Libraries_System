4a10802c96fe545fcbb593d37e112025516d7513
Atomic-Kai <eamputvhichika966@gmail.com>
2025-10-29 17:19:43 +0700

    some route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('master');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('/index');
