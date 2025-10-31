0bb5f1087094f0c5a7856d723e65a01febb7cf0a
Keomhong168 <suykimhong168@gmail.com>
2025-10-31 16:42:46 +0700

    first love

--- backend/database/migrations/2025_10_31_060014_create_genders_table.php
+++ backend/database/migrations/2025_10_31_060014_create_genders_table.php
@@ -3,22 +3,20 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up(): void
     {
         Schema::create('genders', function (Blueprint $table) {
-            $table->id();
-            $table->string('gender_name'); // Male / Female
-            $table->timestamps();
+            $table->id(); // gender id
+            $table->string('gender'); // e.g., Male, Female
         });
 
         // Insert default genders
         DB::table('genders')->insert([
-            ['gender_name' => 'Male', 'created_at' => now(), 'updated_at' => now()],
-            ['gender_name' => 'Female', 'created_at' => now(), 'updated_at' => now()],
+            ['gender' => 'Male'],
+            ['gender' => 'Female']
         ]);
     }
 
